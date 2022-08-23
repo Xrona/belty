511ee136884453957b07337d4630b1ec9ab11864
erxweo <yizzr81@gmailc.on>
2022-08-23 19:59:13 +0400

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import {Button} from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+        <Button variant="contained">Hello world</Button>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, explicabo!</p>
+    </div>
+  )
+}
