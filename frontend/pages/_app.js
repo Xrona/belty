89152e86ee5aa03d8728195b15b728a09706ed24
Alladeliel <balanovich97@gmail.com>
2022-08-28 18:04:56 +0300

    test

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,11 +2,13 @@ import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material'
 
 import { theme } from '../config/theme'
+import Header from '../components/common/header/header'
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider theme={theme}>
 			<Component {...pageProps} />
+			<Header />
 		</ThemeProvider>
 	)
 }
